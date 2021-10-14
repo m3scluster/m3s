419b6b56553aaffa3a31877896ce7a74220991a6
Andreas Peters <ap@aventer.biz>
2021-10-14 12:05:39 +0200

    remove: unneeded api calls.

--- api/api.go
+++ api/api.go
@@ -23,10 +23,9 @@ func SetConfig(cfg *cfg.Config) {
 func Commands() *mux.Router {
 	rtr := mux.NewRouter()
 	rtr.HandleFunc("/v0/agent/scale/{count}", V0ScaleK3SAgent).Methods("GET")
-	rtr.HandleFunc("/v0/agent/reflate", V0ReflateK3SAgent).Methods("GET")
 	rtr.HandleFunc("/v0/server/scale/{count}", V0ScaleK3S).Methods("GET")
-	rtr.HandleFunc("/v0/server/reflate", V0ReflateK3S).Methods("GET")
 	rtr.HandleFunc("/v0/server/config", V0GetKubeconfig).Methods("GET")
+	rtr.HandleFunc("/v0/server/version", V0GetKubeVersion).Methods("GET")
 	rtr.HandleFunc("/v0/etcd/scale/{count}", V0ScaleEtcd).Methods("GET")
 	rtr.HandleFunc("/v0/task/kill/{id}", V0KillTask).Methods("GET")
 	rtr.HandleFunc("/v0/status/m3s", V0StatusM3s).Methods("GET")
