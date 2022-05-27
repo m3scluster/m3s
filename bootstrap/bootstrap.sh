bdbc1037ac815fcee9bf274b51da9fe0c5a24b92
Andreas Peters <ap@aventer.biz>
2022-05-27 12:39:58 +0200

    FIX: boostrap branch failure.

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -10,7 +10,7 @@ export INSTALL_K3S_VERSION=$KUBERNETES_VERSION+k3s1
 export INSTALL_K3S_SKIP_ENABLE=true
 export INSTALL_K3S_SKIP_START=true
 export KUBECONFIG=$MESOS_SANDBOX/kubeconfig.yaml
-export BRANCH=dev
+export BRANCH=master
 
 ## Export json as environment variables
 ## example: MESOS_SANDBOX_VAR='{ "CUSTOMER":"test-ltd" }'
