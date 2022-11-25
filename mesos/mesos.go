b0e152f7a53f80083d62c8cc71011e7c2b2a7620
Andreas Peters <ap@aventer.biz>
2022-11-25 07:15:38 +0100

    ADD: nolint flag.

--- mesos/mesos.go
+++ mesos/mesos.go
@@ -88,6 +88,7 @@ func (e *Mesos) Call(message *mesosproto.Call) error {
 	body, _ := marshaller.MarshalToString(message)
 
 	client := &http.Client{}
+	// #nosec G402
 	client.Transport = &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
@@ -166,6 +167,7 @@ func (e *Mesos) DeclineOffer(offerIds []mesosproto.OfferID) *mesosproto.Call {
 }
 
 // IsRessourceMatched - check if the ressources of the offer are matching the needs of the cmd
+// nolint:gocycle
 func (e *Mesos) IsRessourceMatched(ressource []mesosproto.Resource, cmd cfg.Command) bool {
 	mem := false
 	cpu := false
@@ -247,6 +249,7 @@ func (e *Mesos) GetNetworkInfo(taskID string) []mesosproto.NetworkInfo {
 // GetAgentInfo get information about the agent
 func (e *Mesos) GetAgentInfo(agentID string) cfg.MesosSlaves {
 	client := &http.Client{}
+	// #nosec G402
 	client.Transport = &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
